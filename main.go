13d866e07a7cac3ec1791d05942211f1d3181903
Adam <adam.fadhil@xendit.co>
2022-08-24 12:11:49 +0700

    Return non 0 exit code

--- main.go
+++ main.go
@@ -13,29 +13,32 @@ func main() {
 	// Load file
 	openApiSpec, err := openapi3.NewLoader().LoadFromFile("openapi.yaml")
 	if err != nil {
-                log.Fatal("Failed to load OpenApi spec file")
+		log.Fatal("Failed to load OpenApi spec file")
 	}
 
 	// Validate the spec
 	ctx := context.Background()
 	if err := openApiSpec.Validate(ctx); err != nil {
-                log.Fatal("Bad OpenApi spec")
+		log.Fatal("Bad OpenApi spec")
 	}
 
 	openApiJSON, err := openApiSpec.MarshalJSON()
 	if err != nil {
-                log.Fatal("Failed to marshal")
+		log.Fatal("Failed to marshal")
 	}
 
 	var s Spec
 	if err := json.Unmarshal(openApiJSON, &s); err != nil {
-                log.Fatalf("Failed to marshal OpenApi spec, most likely bad X-Test-Suite format\nErr: %v", err)
+		log.Fatalf("Failed to marshal OpenApi spec, most likely bad X-Test-Suite format\nErr: %v", err)
 	}
 
-        report, err := exec(s)
-        if err  != nil {
-                log.Fatalf("Failed to process test suite. Err: %v", err)
-        }
+	report, err := exec(s)
+	if err != nil {
+		log.Fatalf("Failed to process test suite. Err: %v", err)
+	}
 
-        report.generate()
+	areAllSuccess := report.generate()
+	if !areAllSuccess {
+		log.Fatalf("There are failed test suites")
+	}
 }
--- report.go
+++ report.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/olekukonko/tablewriter"
-        "github.com/fatih/color"
 )
 
 type testResult int
@@ -27,45 +27,44 @@ func (t testResult) String() string {
 }
 
 type actualExpectStatusCodes struct {
-        Expected int
-        Actual int
+	Expected int
+	Actual   int
 }
 
 func (a actualExpectStatusCodes) Match() bool {
-    return a.Expected == a.Actual
+	return a.Expected == a.Actual
 }
 
 type actualExpectBody struct {
-        Expected string
-        Actual string
+	Expected string
+	Actual   string
 }
 
 func (a actualExpectBody) Match() bool {
-    return a.Expected == a.Actual
+	return a.Expected == a.Actual
 }
 
 type resultDetails struct {
-        HTTPStatusCodes actualExpectStatusCodes
-        Body actualExpectBody
+	HTTPStatusCodes actualExpectStatusCodes
+	Body            actualExpectBody
 }
 
 func (rd *resultDetails) SetActualExpectHTTPStatus(expected, actual int) {
-        rd.HTTPStatusCodes.Expected = expected
-        rd.HTTPStatusCodes.Actual = actual
+	rd.HTTPStatusCodes.Expected = expected
+	rd.HTTPStatusCodes.Actual = actual
 }
 
 func (rd *resultDetails) SetActualExpectBody(expected, actual string) {
-        var p1 bytes.Buffer
-        err := json.Indent(&p1, []byte(expected), "", "  ")
-        if err != nil {
-                rd.Body.Expected = expected
-        } else {
-                rd.Body.Expected = p1.String()
+	var p1 bytes.Buffer
+	err := json.Indent(&p1, []byte(expected), "", "  ")
+	if err != nil {
+		rd.Body.Expected = expected
+	} else {
+		rd.Body.Expected = p1.String()
 
-        }
+	}
 
-
-        rd.Body.Actual = actual
+	rd.Body.Actual = actual
 }
 
 type testSuiteReport struct {
@@ -73,26 +72,26 @@ type testSuiteReport struct {
 	Description   string
 	Operation     string
 	Status        testResult
-        ResultDetails resultDetails
+	ResultDetails resultDetails
 
-        FailureReason string
+	FailureReason string
 
-        // This error is caused by http client returning error, we cannot assert the statusCodes
-        // or bodies if there are error in the http call
-        Err error
+	// This error is caused by http client returning error, we cannot assert the statusCodes
+	// or bodies if there are error in the http call
+	Err error
 }
 
 func (t testSuiteReport) IsPassed() bool {
-    return t.Status == testResultPassed
+	return t.Status == testResultPassed
 }
 
 func (t *testSuiteReport) Fail() {
-        t.Status = testResultFailed
+	t.Status = testResultFailed
 }
 
 func (t *testSuiteReport) FailWithError(err error) {
-        t.Err = err
-        t.Fail()
+	t.Err = err
+	t.Fail()
 }
 
 func (t *testSuiteReport) Pass() {
@@ -103,106 +102,106 @@ type Report struct {
 	TestSuites []testSuiteReport
 }
 
-func (r Report) AllSuccess() bool {
-        for _, testSuiteReport := range r.TestSuites {
-            if !testSuiteReport.IsPassed() {
-                    return false
-            }
-        }
-        return true
+func (r Report) AreAllSuccess() bool {
+	for _, testSuiteReport := range r.TestSuites {
+		if !testSuiteReport.IsPassed() {
+			return false
+		}
+	}
+	return true
 }
-
 
 func (r Report) generateFailingTestDescriptions() {
 
-        fmt.Println()
-        fmt.Println()
+	fmt.Println()
+	fmt.Println()
 
-        for _, testSuiteReport := range r.TestSuites {
+	for _, testSuiteReport := range r.TestSuites {
 
-                if !testSuiteReport.IsPassed() {
+		if !testSuiteReport.IsPassed() {
 
-                        color.New(color.FgRed).Add(color.Bold).Printf("• %s > %s > %s\n\n", testSuiteReport.PathName, testSuiteReport.Operation, testSuiteReport.Description)
+			color.New(color.FgRed).Add(color.Bold).Printf("• %s > %s > %s\n\n", testSuiteReport.PathName, testSuiteReport.Operation, testSuiteReport.Description)
 
-                        if testSuiteReport.Err != nil {
-                                fmt.Printf("\t Received error: %s\n\n", testSuiteReport.Err.Error())
-                                continue
-                        }
+			// If err is not nil, we should skip assertion and just explain what the errror is to the user
+			if testSuiteReport.Err != nil {
+				fmt.Printf("\t Received error: %s\n\n", testSuiteReport.Err.Error())
+				continue
+			}
 
-                        var p1 = fmt.Sprintf("Expected status code: \t%d\nActual status code: \t%d\n\n", 
-                            testSuiteReport.ResultDetails.HTTPStatusCodes.Expected,
-                            testSuiteReport.ResultDetails.HTTPStatusCodes.Actual)
-                        if testSuiteReport.ResultDetails.HTTPStatusCodes.Match() {
-                                color.Green(p1)
-                        } else {
-                                color.Red(p1)
-                        }
+			var p1 = fmt.Sprintf("Expected status code: \t%d\nActual status code: \t%d\n\n",
+				testSuiteReport.ResultDetails.HTTPStatusCodes.Expected,
+				testSuiteReport.ResultDetails.HTTPStatusCodes.Actual)
+			if testSuiteReport.ResultDetails.HTTPStatusCodes.Match() {
+				color.Green(p1)
+			} else {
+				color.Red(p1)
+			}
 
+			var p2 = fmt.Sprintf("Expected body: \n\n%s\n\nActual body: \n\n%s\n",
+				testSuiteReport.ResultDetails.Body.Expected, testSuiteReport.ResultDetails.Body.Actual)
+			if testSuiteReport.ResultDetails.Body.Match() {
+				color.Green(p2)
+			} else {
+				color.Red(p2)
+			}
 
-                        var p2 = fmt.Sprintf("Expected body: \n\n%s\n\nActual body: \n\n%s\n", 
-                            testSuiteReport.ResultDetails.Body.Expected, testSuiteReport.ResultDetails.Body.Actual)
-                        if testSuiteReport.ResultDetails.Body.Match() {
-                                color.Green(p2)
-                        } else {
-                                color.Red(p2)
-                        }
-
-                        fmt.Println()
-                }
-        }
+			fmt.Println()
+		}
+	}
 }
 
+func (r Report) generate() bool {
 
-func (r Report) generate() {
+	data := [][]string{}
 
-        data := [][]string{}
+	for _, testSuiteReport := range r.TestSuites {
+		data = append(data, []string{
+			testSuiteReport.PathName,
+			testSuiteReport.Operation,
+			testSuiteReport.Description,
+			testSuiteReport.Status.String(),
+		})
+	}
 
-        for _, testSuiteReport := range r.TestSuites {
-            data = append(data, []string{
-                testSuiteReport.PathName,
-                testSuiteReport.Operation,
-                testSuiteReport.Description,
-                testSuiteReport.Status.String(),
-            })
-        }
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Path", "Operation", "Desc", "Status"})
+	table.SetAutoMergeCellsByColumnIndex([]int{0})
+	table.SetRowLine(true)
+	for i, row := range data {
 
-        table := tablewriter.NewWriter(os.Stdout)
-        table.SetHeader([]string{"Path", "Operation", "Desc", "Status"})
-        table.SetAutoMergeCellsByColumnIndex([]int{0})
-        table.SetRowLine(true)
-        for i,  row := range data {
+		// Row 3 is STATUS -- Refer to SetHeader function
+		if row[3] == "FAILED" {
 
-            // Row 3 is STATUS -- Refer to SetHeader function
-            if row[3] == "FAILED" {
+			// The function `Rich` also appends data to tablewriter
+			// so we do not need to manually append again
+			table.Rich(data[i], []tablewriter.Colors{
+				{},
+				{},
+				{},
+				{
+					// If test is failing, we mark the cell as RED color
+					tablewriter.Bold, tablewriter.BgRedColor,
+				},
+			})
 
-                    // The function `Rich` also appends data to tablewriter
-                    // so we do not need to manually append again
-                    table.Rich(data[i], []tablewriter.Colors{
-                        {},
-                        {},
-                        {},
-                        {
-                            // If test is failing, we mark the cell as RED color
-                            tablewriter.Bold, tablewriter.BgRedColor,
-                        },
-                    })
+		} else {
+			table.Rich(data[i], []tablewriter.Colors{
+				{},
+				{},
+				{},
+				{
+					// If test is failing, we mark the cell as GREEN color
+					tablewriter.Bold, tablewriter.FgGreenColor,
+				},
+			})
 
-            } else {
-                    table.Rich(data[i], []tablewriter.Colors{
-                        {},
-                        {},
-                        {},
-                        {
-                            // If test is failing, we mark the cell as GREEN color
-                            tablewriter.Bold, tablewriter.FgGreenColor,
-                        },
-                    })
+		}
 
-            }
+	}
 
-        }
+	table.Render()
 
-        table.Render()
+	r.generateFailingTestDescriptions()
 
-        r.generateFailingTestDescriptions()
+	return r.AreAllSuccess()
 }
