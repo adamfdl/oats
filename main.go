77e78f721f4f4511da4c3675d6c23ef130ff0a89
Adam <adam.fadhil@xendit.co>
2022-09-13 09:12:36 +0700

    Add versioning flag

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 )
 
+const version = "v0.1.4"
+
 func main() {
+
+	var versionFlag bool
+	flag.BoolVar(&versionFlag, "v", false, "oatsy -v")
 
 	var openApiFilePath string
 	flag.StringVar(&openApiFilePath, "f", "", "-f path/to/openapi.yaml")
 	flag.Parse()
+
+	if versionFlag {
+		fmt.Printf("Running version %v\n", version)
+		os.Exit(0)
+	}
 
 	if openApiFilePath == "" {
 		log.Fatalf("No file is specified. Ex oatsy -f path/to/openapi.yaml")
